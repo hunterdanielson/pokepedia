7bcfbc45ca15d9a7a5e5dc302e6a993ed098523d
Hunter Danielson <hunterleedanielson@gmail.com>
2020-08-04 00:45:58 -0700

    header and list test

--- src/reducers/appReducer.test.js
+++ src/reducers/appReducer.test.js
@@ -32,5 +32,64 @@ describe('app reducer', () => {
       pageNum: 3
     });
   });
+
+  it('sets the list with the SET_LIST action type', () => {
+    const state = {
+      list: []
+    };
+
+    const action = {
+      type: 'SET_LIST',
+      payload: {
+        results: [
+          { 
+            name: 'bulbasaur',
+            url: 'bulbasaur.com'
+          }
+        ]
+      }
+    };
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual({
+      list: [{
+        name: 'bulbasaur',
+        url: 'bulbasaur.com'
+      }]
+    });
+  });
+
+  it('toggles the theme from light to dark', () => {
+    const state = {
+      theme: 'light'
+    };
+
+    const action = {
+      type: 'TOGGLE_THEME'
+    };
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual({
+      theme: 'dark'
+    });
+  });
   
+  it('toggles the theme from dark to light', () => {
+    const state = {
+      theme: 'dark'
+    };
+
+    const action = {
+      type: 'TOGGLE_THEME'
+    };
+
+    const newState = reducer(state, action);
+    
+    expect(newState).toEqual({
+      theme: 'light'
+    });
+  });
+
 });
