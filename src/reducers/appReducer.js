6b1f7a70f1d3e9ca9760e9220465f1daa2d799e6
Hunter Danielson <hunterleedanielson@gmail.com>
2020-08-04 00:14:49 -0700

    base functionality working

---
+++ src/reducers/appReducer.js
@@ -0,0 +1,21 @@
+export const initialState = {
+  pageNum: 1,
+  totalPages: 21,
+  list: [],
+  theme: 'light'
+};
+
+export default function reducer(state, action) {
+  switch(action.type) {
+    case 'NEXT_PAGE': 
+      return { ...state, pageNum: state.pageNum + 1 };
+    case 'PREVIOUS_PAGE':
+      return { ...state, pageNum: state.pageNum - 1 };
+    case 'SET_LIST': 
+      return { ...state, list: action.payload.results };
+    case 'TOGGLE_THEME':
+      return { ...state, theme: state.theme === 'light' ? 'dark' : 'light' };
+    default: 
+      return state;
+  }
+}
